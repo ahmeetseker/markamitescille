9e5b90c092a8fa29cb9e499f2c12e790a61e3243
ahmetseker <ahmetseker1905@hotmail.com.tr>
2023-12-27 17:26:53 +0300

    Allpageupdate

---
+++ src/components/BlogDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BlogDetail = () => {
+ return (
+  <div className="container mx-auto">
+   <h1 className="">MARKA YENİLEME (NORMAL)</h1>
+   <p>aciklama aciklama aciklama aciklama aciklama aciklama aciklama aciklama aciklama aciklama aciklama aciklama aciklama</p>
+  </div>
+ );
+};
+
+export default BlogDetail;
